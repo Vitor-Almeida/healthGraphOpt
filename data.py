07e25a0cfa075e838b0c915c1c415cfa1ef3f259
Vitor Almeida <almeida.f.vitor@gmail.com>
2022-09-06 20:14:12 -0300

    working example from article

---
+++ data.py
@@ -0,0 +1,56 @@
+import pandas as pd
+
+
+class graph_data():
+
+    def __init__(self):
+
+
+        dfdHospital = pd.read_csv('/home/jaco/Projetos/healthGraphOpt/data/dHospital.csv')
+        dfmOD = pd.read_csv('/home/jaco/Projetos/healthGraphOpt/data/mOD.csv')
+        dffPaciente = pd.read_csv('/home/jaco/Projetos/healthGraphOpt/data/fPaciente.csv')
+
+        dfmOD = dfmOD.melt(id_vars='areaId',var_name='hosId',value_name='dist')
+        dfmOD['areaId']=dfmOD['areaId'].astype(int)
+        dfmOD['hosId']=dfmOD['hosId'].astype(int)
+        dfmOD['dist']=dfmOD['dist'].astype(float)
+        dfmOD.set_index(['areaId','hosId'],inplace=True)
+
+        
+        dffPaciente['areaId']=dffPaciente['areaId'].astype(int)
+        dffPaciente['patTypeId']=dffPaciente['patTypeId'].astype(int)
+        dffPaciente['qty']=dffPaciente['qty'].astype(int)
+
+        dffDemanda = dffPaciente.groupby(by=['dia','areaId','areaName','patTypeId','patTypeName']).agg({'qty':'sum'}).reset_index()
+        dffDemanda.set_index(['patTypeId','areaId','dia'],inplace=True)
+  
+
+        dfCONcapacity = dfdHospital.groupby(by=['hosId','hosName','equipId','equipName']).agg({'initialEquipCap':'sum'}).reset_index()
+        dfCONcapacity.set_index(['equipId','hosId'],inplace=True)
+
+        dfInitPatientsph = dfdHospital.groupby(by=['hosId','hosName','initalPatType']).agg({'initialPat':'sum','ReleasePatt':'sum'}).reset_index()
+        dfInitPatientsph.set_index(['initalPatType','hosId'],inplace=True)
+
+        dfAvgLenStay = dffPaciente[['patTypeId','AvgLenStay']].drop_duplicates()
+        dfAvgLenStay.set_index(['patTypeId'],inplace=True)
+
+    
+        #dfmOD.reset_index(inplace=True)
+
+        self.patTypeList = list(dffPaciente['patTypeId'].drop_duplicates())
+        self.areaIdList = list(dffPaciente['areaId'].drop_duplicates())
+        self.hosIdList = list(dfdHospital['hosId'].drop_duplicates())
+        self.equipIdList = list(dfdHospital['equipId'].drop_duplicates())
+        self.tList = list(dffPaciente['dia'].drop_duplicates())
+
+        self.Demandpat = {index:row['qty'] for index, row in dffDemanda.iterrows()}
+
+        self.CONCapacityrh  = {index:row['initialEquipCap'] for index, row in dfCONcapacity.iterrows()} 
+
+        self.InitPatientsph  = {index:row['initialPat'] for index, row in dfInitPatientsph.iterrows()} 
+
+        self.releasePatientsph  = {index:row['ReleasePatt'] for index, row in dfInitPatientsph.iterrows()}
+
+        self.LOSp = {index:row['AvgLenStay'] for index, row in dfAvgLenStay.iterrows()}
+
+        self.Distanceah = {index:row['dist'] for index, row in dfmOD.iterrows()}
--- main.py
+++ main.py
@@ -1,22 +1,77 @@
-from pyomo.environ import *
+import pyomo.environ as pyo
+from data import graph_data
 
-model = ConcreteModel()
+data = graph_data()
+Demandpat = data.Demandpat
+CONCapacityrh  = data.CONCapacityrh
+InitPatientsph  = data.InitPatientsph
+releasePatientsph  = data.releasePatientsph
+LOSp = data.LOSp
+Distanceah = data.Distanceah
 
-# declare decision variables
-model.x = Var(domain=NonNegativeReals)
+model = pyo.ConcreteModel()
+model.dual = pyo.Suffix(direction=pyo.Suffix.IMPORT)
 
-# declare objective
-model.profit = Objective(
-    expr = 40*model.x,
-    sense = maximize)
+# Step 1: Define index sets
+patTypeList = data.patTypeList
+equipTypeList = data.equipIdList
+areaIdList = data.areaIdList
+hosIdList = data.hosIdList
+tList = data.tList
 
-# declare constraints
-model.demand = Constraint(expr = model.x <= 40)
-model.laborA = Constraint(expr = model.x <= 80)
-model.laborB = Constraint(expr = 2*model.x <= 100)
+# Step 2: Define the decision 
+model.x = pyo.Var(patTypeList, areaIdList, hosIdList, tList, domain = pyo.NonNegativeReals) # (6)
+model.y = pyo.Var(patTypeList, hosIdList, tList, domain = pyo.NonNegativeReals)
 
-# solve
-SolverFactory('cbc', executable='/usr/bin/cbc').solve(model).write()
+# Step 3: Define Objective
+model.Cost = pyo.Objective(
+    expr = sum([Distanceah[a,h]*model.x[p,a,h,t] for p in patTypeList for a in areaIdList for h in hosIdList for t in tList]),
+    sense = pyo.minimize)
 
-print("Profit = ", model.profit(), " per week")
-print("X = ", model.x(), " units per week")
+# Step 4: Constraints
+model.demand = pyo.ConstraintList() #(2) do artigo, quantidade de solucoes por trecho nao pode ser maior que a demanda.
+for p in patTypeList:
+    for a in areaIdList:
+        for t in tList:
+            model.demand.add(sum([model.x[p,a,h,t] for h in hosIdList]) == Demandpat[p,a,t] )
+
+def noPatN(p,h,t):
+
+    if t == 0:
+        return InitPatientsph[p,h] + sum([model.x[p,a,h,0] for a in areaIdList])
+    else:
+        return noPatN(p,h,t-1) + sum([model.x[p,a,h,t] for a in areaIdList]) - \
+               sum([model.x[p,a,h,t] - LOSp[p] for a in areaIdList]) - \
+               releasePatientsph[p,h] #no artigo é por t (faz sentido?)
+
+model.noPattN = pyo.ConstraintList() #(3,4) do artigo, quantidade de pacientes no instante t é igual ao pacientes t-1 + alocao atual.
+for p in patTypeList:
+    for h in hosIdList:
+        for t in tList:
+                model.noPattN.add(model.y[p,h,t] == noPatN(p,h,t))
+
+model.equipLimit = pyo.ConstraintList() #(5) do artigo, quantidade de pacientes nao pode ser maior que a quantidade de equipamentos.
+
+for r in equipTypeList:
+    for h in hosIdList:
+            model.equipLimit.add(sum([model.y[r,h,t] for t in tList]) <= CONCapacityrh[r,h])
+
+
+#precisa? x ja foi definido como real nao negativo la em cima.
+#model.positivo = ConstraintList() #(6) do artigo, alocação precisa ser positiva
+#model.positivo.add(sum([model.x[p,a,h,t] for p in patTypeList for a in areaIdList for h in hosIdList for t in tList]) >= 0)
+
+results = pyo.SolverFactory('cbc').solve(model)
+results.write()
+
+if 'ok' == str(results.Solver.status):
+    print("Total Shipping Costs = ",model.Cost())
+    print("\nShipping Table:")
+    for p in patTypeList:
+        for a in areaIdList:
+            for h in hosIdList:
+                for t in tList:
+                    if model.x[p,a,h,t]() > 0:
+                        print(f"Ship from {a} to {h} patient type {p} at day {t} : {model.x[p,a,h,t]()}")
+else:
+    print("No Valid Solution Found")
