238e8ee38cd82246255e2fb50f4b685393df3cd9
Vitor Almeida <almeida.f.vitor@gmail.com>
2022-09-07 17:11:10 -0300

    v0.1 data from sus class working

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ tList = data.tList
 model.x = pyo.Var(patTypeList, areaIdList, hosIdList, tList, domain = pyo.PositiveReals) # (6)
 model.y = pyo.Var(patTypeList, hosIdList, tList, domain = pyo.PositiveReals)
 
-
 # Step 3: Define Objective
 model.Cost = pyo.Objective(
     expr = sum([Distanceah[a,h]*model.x[p,a,h,t] for p in patTypeList for a in areaIdList for h in hosIdList for t in tList]),
@@ -40,24 +39,26 @@ def noPatN(p,h,t):
 
     if t == 0:
         return InitPatientsph[p,h] + sum([model.x[p,a,h,0] for a in areaIdList])
-    else:
+    elif t - LOSp[p] >= 0:
         return noPatN(p,h,t-1) + sum([model.x[p,a,h,t] for a in areaIdList]) - \
                sum([model.x[p,a,h,t- LOSp[p]]  for a in areaIdList]) - \
-               releasePatientsph[p,h] #no artigo é por ,t (faz sentido?)
+               releasePatientsph[p,h] 
+    else:
+        return noPatN(p,h,t-1) + sum([model.x[p,a,h,t] for a in areaIdList]) - \
+        0 - \
+        releasePatientsph[p,h] 
 
 model.noPattN = pyo.ConstraintList() #(3,4) do artigo, quantidade de pacientes no instante t é igual ao pacientes t-1 + alocao atual.
 for p in patTypeList:
     for h in hosIdList:
         for t in tList:
-                model.noPattN.add(model.y[p,h,t] == noPatN(p,h,t))
-
+            model.noPattN.add(model.y[p,h,t] == noPatN(p,h,t))
 
 model.equipLimit = pyo.ConstraintList() #(5) do artigo, quantidade de pacientes nao pode ser maior que a quantidade de equipamentos.
 
 for r in equipTypeList:
     for h in hosIdList:
             model.equipLimit.add(sum([model.y[r,h,t] for t in tList]) <= CONCapacityrh[r,h])
-
 
 #precisa? x ja foi definido como real nao negativo la em cima.
 model.positivo = pyo.ConstraintList() #(6) do artigo, alocação precisa ser positiva
@@ -79,7 +80,6 @@ if 'ok' == str(results.Solver.status):
         for p in patTypeList:
             for h in hosIdList:
                 for a in areaIdList:
-                    #if model.y[p,h,t]() != 0 and model.x[p,a,h,t]() != 0:
                     if model.x[p,a,h,t]() > 0:
                         print(f"At day {t} patient type {p} to hospital {h} from area {a} : {model.x[p,a,h,t]()} | NoPatients pht: {model.y[p,h,t]()} | custo: {Distanceah[a,h]*model.x[p,a,h,t]()} | capacity: {CONCapacityrh[p,h]} | demanda: {Demandpat[p,a,t]}")
 else:
---
+++ utils.py
@@ -0,0 +1,22 @@
+from math import sin, cos, sqrt, atan2, radians
+
+def distance_latLong(lat1,lon1,lat2,lon2):
+
+    # approximate radius of earth in km
+    R = 6373.0
+
+    lat1 = radians(lat1)
+    lon1 = radians(lon1)
+    lat2 = radians(lat2)
+    lon2 = radians(lon2)
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    distance = R * c
+
+    return distance
+
